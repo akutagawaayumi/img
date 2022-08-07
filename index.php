2c40bb391942162716697c641f7d849e10dd0731
ayumi akutagawa <92968228+akutagawaayumi@users.noreply.github.com>
2022-08-07 18:53:02 +0900

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ fclose($fp);
 <html lang="ja">
 
 <head>
-    <title>Title | The Things I (We) Own</title>
+    <title>the best items</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
