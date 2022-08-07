50c3a9279e837303c8dc3def4ad6ba39a11da282
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-08 01:44:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ $(document).ready(function(event) {
 
     volume = new Tone.Volume(-10);
     synth = new Tone.PolySynth(10, Tone.Synth).chain(volume, Tone.Master);
-    notes = Tone.Frequency("E5").harmonize([3, 2, 5, 6, 7, 10, 11, 13,]);
+    notes = Tone.Frequency("E5").harmonize([13, 12, 15, 16, 17, 110, 111, 113,]);
 });
 
 $("#marquee").click(function(e) {
