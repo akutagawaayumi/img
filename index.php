d399e6b76708e6d317b5e48325ac1930f9f3c293
ayumi akutagawa <92968228+akutagawaayumi@users.noreply.github.com>
2022-08-11 17:43:31 +0900

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ fclose($fp);
         h1,
         h2,
         .collection h2 {
-            font-family: "Cochin", MS Serif; serif;
+            font-family: "kia-Regular_Extended, Skia; font-stretch: expanded", MS Serif; serif;
             font-weight: 300;
             font-stretch: ;
             font-variant: common-ligatures tabular-nums;
