5645aa2ffca50b233fc4a2a9dfba31112c14df59
ayumi akutagawa <92968228+akutagawaayumi@users.noreply.github.com>
2022-08-11 17:41:31 +0900

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ fclose($fp);
         h1,
         h2,
         .collection h2 {
-            font-family: "Big Caslon", MS Serif; serif;
+            font-family: "Cochin", MS Serif; serif;
             font-weight: 300;
             font-stretch: ;
             font-variant: common-ligatures tabular-nums;
