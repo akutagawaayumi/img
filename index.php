91e52198f003f2d8902fec5ca28ced07faec8d47
ayumi akutagawa <92968228+akutagawaayumi@users.noreply.github.com>
2022-08-11 17:37:56 +0900

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ fclose($fp);
         h1,
         h2,
         .collection h2 {
-            font-family: "Papyrus", MS Serif; serif;
+            font-family: "Shippori Mincho", MS Serif; serif;
             font-weight: 300;
             font-stretch: ;
             font-variant: common-ligatures tabular-nums;
